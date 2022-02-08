b8a6972a00a3d6010dfd722cd74b1c9f42cb20bc
broliveira06 <brunobrstecnologia@gmail.com>
2022-02-08 20:09:26 -0300

    Desafio de accordion concluido

---
+++ Bootcamps/Take Blip Web Developer/Primeiros passos Desenvolvimento Web/index2.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Desafio Accordion</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+  <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.min.js"></script>
+  <script>
+  $( function() {
+    $( "#accordion" ).accordion();
+  } );
+  </script>
+</head>
+<body>
+ 
+<div id="accordion">
+		<h3>Desafio 1</h3>
+	<div>
+		<p>TESTE DE ACCORDION 1</p>
+	</div>
+	<div>
+	<h3>Desafio 2</h3>
+    <p> TESTE DE ACORDION 2</p>
+	</div>
+	</div>
+ 
+</body>
+	
+</html>
