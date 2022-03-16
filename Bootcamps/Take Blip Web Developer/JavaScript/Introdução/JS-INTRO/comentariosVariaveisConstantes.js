723f275aa0d71e840d5e59b2ee554c4bd9740322
broliveira06 <brunobrstecnologia@gmail.com>
2022-03-16 11:31:27 -0300

    Projeto Contador

---
+++ Bootcamps/Take Blip Web Developer/JavaScript/Introdução/JS-INTRO/comentariosVariaveisConstantes.js
@@ -0,0 +1,19 @@
+// comentario de uma linha
+/*
+comentário de várias linhas
+*/
+ 
+/*Declarando uma variavel
+
+var preco1 = 2,00;
+var preco2 = 5,00;
+
+*/
+
+/*Declarando uma CONSTANTE
+
+const PRECO1 = 2,00;
+const PRECO2 = 5,00;
+
+é uma boa pratica declara uma váriavel com caracteres em MAIUSCULO para diferenciar uma CONSTANTE de um variavel;*/
+     
---
+++ Bootcamps/Take Blip Web Developer/JavaScript/Introdução/JS-INTRO/funções.js
@@ -0,0 +1,27 @@
+//Declarando um função de soma  
+function soma(a,b /* Aqui vai os parametros*/){
+//aqui vai o que a função vai fazer
+
+//console.log(a + b) retorna o valor no console
+return a + b;
+
+}
+
+//Fazer a chamada da função
+
+soma(3/*Aqui vai o valor do primeiro parametro*/,5/*Aqui vai o valor do segundo parametro*/);  
+
+
+
+//sintax 
+
+//Declarando uma função
+
+/*function nome (parametros){
+
+}
+*/
+
+//Chamada de função
+
+/*nome(valor dos     parametros)*/
