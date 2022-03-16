544bcd051fdaa2112c646629d332a420bd345937
broliveira06 <brunobrstecnologia@gmail.com>
2022-03-16 12:16:37 -0300

    Implemetação do método addEventListener

--- Bootcamps/Take Blip Web Developer/JavaScript/Introdução/Projeto-Contador/assets/js/scripts.js
+++ Bootcamps/Take Blip Web Developer/JavaScript/Introdução/Projeto-Contador/assets/js/scripts.js
@@ -1,12 +1,31 @@
 var currentNumberWarpper = document.getElementById('currentNumber');
 var currentNumber = 0;
 
-function increment(){
-    currentNumber = currentNumber + 1;
-    currentNumberWarpper.innerHTML = currentNumber;
-}
+var mais = document.getElementById('adicionar');
+var menos = document.getElementById('subtrair');
 
-function decrement(){
-    currentNumber = currentNumber - 1;
-    currentNumberWarpper.innerHTML = currentNumber;
-}
+mais.addEventListener("click", increment);
+menos.addEventListener("click", decrement);
+
+    function increment(){
+        if(currentNumber < -1){
+            currentNumberWarpper.style.color = 'red';
+        }else{
+            currentNumberWarpper.style.color = 'green'
+        } 
+        if(currentNumber < 11 ){
+            currentNumber = currentNumber + 1;
+            currentNumberWarpper.innerHTML = currentNumber;
+
+        }
+    }
+
+    function decrement(){
+        if(currentNumber <= 0 ){
+            currentNumberWarpper.style.color = 'red';
+        }else{
+            currentNumberWarpper.style.color = 'black';
+        }
+            currentNumber =  currentNumber = currentNumber -1;
+            currentNumberWarpper.innerHTML = currentNumber;
+       }
