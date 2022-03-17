677f6734562ad2f94bb4b19c5092e9e4d0699656
broliveira06 <brunobrstecnologia@gmail.com>
2022-03-17 17:32:32 -0300

    Calculadora JavaScript

---
+++ Bootcamps/Take Blip Web Developer/Sintaxe Básica Java Script/Aula6-Aprofundando em função/assets/js/script.js
@@ -0,0 +1,34 @@
+// tipos de função 
+
+//funções declarativas
+/*     function funcao(){
+        console.log('Tudo certo até aqui');
+    }
+
+    funcao(); */
+
+//expressão de função 
+//com nomeação
+/* 
+var funcao = function funcao(){
+    console.log('Sua menssagem de função de expressão!');
+}
+
+funcao(); */
+
+//expressão de função 
+//sem nomeação
+
+/* var funcao = function (){
+    console.log('Sua menssagem de função de expressão!');
+}
+
+funcao(); */
+
+//arrow function
+/* var funcao = () =>{
+    console.log('Sou uma arrow function');
+}
+
+funcao(); */
+
